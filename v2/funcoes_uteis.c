b61f25f90d8ed8faab11bd5a836c42e405780360
Marlon Douglas <madougla@c2r2p4.42sp.org.br>
2023-06-10 11:32:57 -0300

    1132

---
+++ v2/funcoes_uteis.c
@@ -0,0 +1,71 @@
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	negative;
+
+	negative = 1;
+	res = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+	return (res * negative);
+}
+
+int	getdec(int nb)
+{
+	if (nb >= 90)
+		return (90);
+	else if (nb >= 80)
+		return (80);
+	else if (nb >= 70)
+		return (70);
+	else if (nb >= 60)
+		return (60);
+	else if (nb >= 50)
+		return (50);
+	else if (nb >= 40)
+		return (40);
+	else if (nb >= 30)
+		return (30);
+	else if (nb >= 20)
+		return (20);
+	else if (nb <= 20)
+		return (nb);
+	else
+		return (0);
+}
+
+int	getmult(int nb)
+{
+	if (nb >= 1000000000)
+		return (1000000000);
+	else if (nb >= 1000000)
+		return (1000000);
+	else if (nb >= 1000)
+		return (1000);
+	else if (nb >= 100)
+		return (100);
+	else
+		return (getdec(nb));
+}
--- v2/main.c
+++ v2/main.c
@@ -5,45 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "funcoes_uteis.c"
 
 typedef struct {
 	long numero;
 	char numero_em_palavras[21];
 } RegistroDicionario;
-
-void	ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-}
-
-int	ft_atoi(const char *str)
-{
-	int	res;
-	int	negative;
-
-	negative = 1;
-	res = 0;
-	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
-			*str == '\v' || *str == '\f' || *str == '\r'))
-		++str;
-	if (*str == '-')
-		negative = -1;
-	if (*str == '-' || *str == '+')
-		++str;
-	while (*str && *str >= '0' && *str <= '9')
-	{
-		res = res * 10 + (*str - 48);
-		++str;
-	}
-	return (res * negative);
-}
 
 void carregueDicionarioNaMemoria(RegistroDicionario *dicionario, int *numero_de_registros, char *arquivo_de_dicionario) {
 	FILE *arquivo = fopen(arquivo_de_dicionario, "r");
@@ -87,44 +54,6 @@ char* receber_numero_em_palavras(RegistroDicionario *dicionario, int numero_de_r
 	}
 
 	return "receber_numero_em_palavras() - numero não achado no dicionário.";
-}
-
-int	getdec(int nb)
-{
-	if (nb >= 90)
-		return (90);
-	else if (nb >= 80)
-		return (80);
-	else if (nb >= 70)
-		return (70);
-	else if (nb >= 60)
-		return (60);
-	else if (nb >= 50)
-		return (50);
-	else if (nb >= 40)
-		return (40);
-	else if (nb >= 30)
-		return (30);
-	else if (nb >= 20)
-		return (20);
-	else if (nb <= 20)
-		return (nb);
-	else
-		return (0);
-}
-
-int	getmult(int nb)
-{
-	if (nb >= 1000000000)
-		return (1000000000);
-	else if (nb >= 1000000)
-		return (1000000);
-	else if (nb >= 1000)
-		return (1000);
-	else if (nb >= 100)
-		return (100);
-	else
-		return (getdec(nb));
 }
 
 void printNumeroEmPalavras(RegistroDicionario *dicionario, int numero_de_registros, long numero) {
